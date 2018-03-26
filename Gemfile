66551e3fc8b0167e92fc903c0c9eb13942b9da63
Russell Trow <russell@dockleafdigital.com>
2018-03-26 17:23:38 +0100

    Added a "database" end-point to test a database connection

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 
 gem "rack"
-gem "rack-test"
 gem "sinatra"
+gem "mysql2"
--- web.rb
+++ web.rb
@@ -1,7 +1,23 @@
 require "sinatra"
+require "socket"
+require "mysql2"
 
 set :bind, "0.0.0.0"
 
 get "/" do
-  "Hello World #{params[:name]}".strip
+  @hostname = Socket.gethostname
+  erb :index
+end
+
+get "/health" do
+  random = rand(1...10)
+
+  return status 500 if random < 4
+  status 200
+end
+
+get "/database" do
+  client = Mysql2::Client.new(host: "mysql", database: "pgk_development", username: ENV['DB_USER'], password: ENV['DB_PASSWORD'])
+  results = client.query("SELECT * FROM users")
+  "#{results.count} rows returned."
 end
